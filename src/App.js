84ca0ae8bd9153094bf10011b464cb99c91788a7
Anastasia Rambaud <anastasia.rambaud@gmail.com>
2022-01-17 14:49:19 +0100

    Add color generator

--- src/App.js
+++ src/App.js
@@ -1,17 +1,38 @@
 import './App.css';
-import { useState } from 'react';
+import React, { useState } from 'react';
 import randomColor from 'randomcolor';
 
 function App() {
   const [color, setColor] = useState('#FFFFFF');
+  const [hue, setHue] = useState();
+  const [luminosity, setLuminosity] = useState();
 
+  function chooseHue(event) {
+    setHue(event.currentTarget.value);
+  }
+  function chooseLuminosity(event) {
+    setLuminosity(event.currentTarget.value);
+  }
   function handleChange() {
-    const newColor = randomColor();
+    // const newHue = window.prompt('Hue ?');
+    // const newLuminosity = window.prompt('Luminosity ?');
+    const newColor = randomColor({
+      luminosity: luminosity,
+      hue: hue,
+    });
     setColor(newColor);
   }
 
   return (
     <div className="App">
+      <label>
+        Choose a hue:
+        <input onChange={chooseHue} value={hue} />
+      </label>
+      <label>
+        Choose a luminosity:
+        <input onChange={chooseLuminosity} value={luminosity} />
+      </label>
       <button onClick={handleChange}>Generate</button>
       <div
         style={{
