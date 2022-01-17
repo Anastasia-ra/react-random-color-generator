72419eacd08156f93055f7a839c5fd80cc470a31
Anastasia Rambaud <anastasia.rambaud@gmail.com>
2022-01-17 17:43:27 +0100

    Add style

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
-import './App.css';
+import {
+  buttonStyle,
+  inputStyle,
+  labelStyle,
+  appStyle,
+  formStyle,
+} from './style.js';
 import React, { useState } from 'react';
 import randomColor from 'randomcolor';
+/** @jsxImportSource @emotion/react */
+import { css } from '@emotion/react';
 
 function App() {
   const [color, setColor] = useState('#FFFFFF');
   const [hue, setHue] = useState();
   const [luminosity, setLuminosity] = useState();
+  const [width, setWidth] = useState('400');
+  const [height, setHeight] = useState('50');
 
   function chooseHue(event) {
     setHue(event.currentTarget.value);
@@ -13,9 +23,14 @@ function App() {
   function chooseLuminosity(event) {
     setLuminosity(event.currentTarget.value);
   }
+  function chooseWidth(event) {
+    setWidth(event.currentTarget.value);
+  }
+  function chooseHeight(event) {
+    setHeight(event.currentTarget.value);
+  }
+
   function handleChange() {
-    // const newHue = window.prompt('Hue ?');
-    // const newLuminosity = window.prompt('Luminosity ?');
     const newColor = randomColor({
       luminosity: luminosity,
       hue: hue,
@@ -24,19 +39,57 @@ function App() {
   }
 
   return (
-    <div className="App">
-      <label>
-        Choose a hue:
-        <input onChange={chooseHue} value={hue} />
-      </label>
-      <label>
+    <div className="App" css={appStyle}>
+      <div css={formStyle}>
+        <label css={labelStyle}>
+          Choose a hue:
+          <input
+            css={inputStyle}
+            className="userInput"
+            onChange={chooseHue}
+            value={hue}
+          />
+        </label>{' '}
+      </div>
+      <label css={labelStyle}>
         Choose a luminosity:
-        <input onChange={chooseLuminosity} value={luminosity} />
+        <input
+          css={inputStyle}
+          className="userInput"
+          onChange={chooseLuminosity}
+          value={luminosity}
+        />
       </label>
-      <button onClick={handleChange}>Generate</button>
+      <label css={labelStyle}>
+        Box width:
+        <input
+          css={inputStyle}
+          className="userInput"
+          onChange={chooseWidth}
+          value={width}
+        />
+      </label>
+      <label css={labelStyle}>
+        Box height:
+        <input
+          css={inputStyle}
+          className="userInput"
+          onChange={chooseHeight}
+          value={height}
+        />
+      </label>
+      <button css={buttonStyle} onClick={handleChange}>
+        Generate
+      </button>
       <div
+        css={labelStyle}
         style={{
+          margin: '20px',
           backgroundColor: color,
+          width: Number(width),
+          height: Number(height),
+          textAlign: 'center',
+          //transition: all 2s ease,
         }}
       >
         Generated color: {color}
---
+++ src/style.js
@@ -0,0 +1,47 @@
+/** @jsxImportSource @emotion/react */
+import { css } from '@emotion/react';
+
+export const appStyle = css`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const formStyle = css`
+  display: flex;
+  align-items: center;
+`;
+
+export const buttonStyle = css`
+  font-size: 1.2em ;
+  text-align: center;
+  background-color: #ea4c89;
+  border-radius: 15px;
+  border-style: none;
+  /* box-sizing: border-box; */
+  color: #ffffff;
+  cursor: pointer;
+  font-family: 'Arial',
+
+  font-weight: 500;
+  height: 50px;
+  width: 100px;
+  padding: 10px 10px;
+
+`;
+
+export const labelStyle = css`
+  display: block;
+  font: Arial, Helvetica, sans-serif;
+  color: #524949;
+  font-size: 2em;
+`;
+
+export const inputStyle = css`
+  border-style: none;
+  border-radius: 10px;
+  box-shadow: 2px 2px 2px 2px #dbdbdb;
+  height: 40px;
+  margin: 20px;
+`;
